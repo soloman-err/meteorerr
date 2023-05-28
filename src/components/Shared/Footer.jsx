057f07450948eca240af922054dd6ee9484a03c6
soloman.err <soloman.err@gmail.com>
2023-05-28 07:12:05 +0600

    footer modified

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="bg-black py-5 px-5">
+      <footer className="bg-black py-5 px-5 text-white">
         <h1 className="text-3xl font-bold">footer</h1>
         <small>license acquired by solo</small>
       </footer>
